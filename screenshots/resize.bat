93330d66f94436e72a0c2d1961528de8581cbdc6
Brad Rushworth <brushworth@bitbot.com.au>
2022-01-13 17:33:56 +1100

    Updating screenshots.

---
+++ screenshots/resize.bat
@@ -0,0 +1,34 @@
+magick "iPad Air 4 [en_AU, ipad-air-4, light, home].png" -resize 2048x2732\! "resized/iPad Air 4 [en_AU, ipad-air-4, light, home].png"
+magick "iPad Air 4 [en_AU, ipad-air-4, light, home] (1).png" -resize 2732x2048\! "resized/iPad Air 4 [en_AU, ipad-air-4, light, home] (1).png"
+magick "iPad Air 4 [en_AU, ipad-air-4, dark, home].png" -resize 2048x2732\! "resized/iPad Air 4 [en_AU, ipad-air-4, dark, home].png"
+magick "iPad Air 4 [en_AU, ipad-air-4, dark, home] (1).png" -resize 2732x2048\! "resized/iPad Air 4 [en_AU, ipad-air-4, dark, home] (1).png"
+
+magick "iPhone SE [en_AU, iphone-se, light, home].png" -resize 1242x2208\! "resized/iPhone SE [en_AU, iphone-se, light, home].png"
+magick "iPhone SE [en_AU, iphone-se, light, home] (1).png" -resize 2208x1242\! "resized/iPhone SE [en_AU, iphone-se, light, home] (1).png"
+magick "iPhone SE [en_AU, iphone-se, dark, home].png" -resize 1242x2208\! "resized/iPhone SE [en_AU, iphone-se, dark, home].png"
+magick "iPhone SE [en_AU, iphone-se, dark, home] (1).png" -resize 2208x1242\! "resized/iPhone SE [en_AU, iphone-se, dark, home] (1).png"
+
+magick "iPhone 13 Pro Max [en_AU, iphone-13-pro-max, light, home].png" -resize 1284x2778\! -alpha off "resized/iPhone 13 Pro Max [en_AU, iphone-13-pro-max, light, home].png"
+magick "iPhone 13 Pro Max [en_AU, iphone-13-pro-max, light, home] (1).png" -resize 2778x1284\! -alpha off "resized/iPhone 13 Pro Max [en_AU, iphone-13-pro-max, light, home] (1).png"
+magick "iPhone 13 Pro Max [en_AU, iphone-13-pro-max, dark, home].png" -resize 1284x2778\! -alpha off "resized/iPhone 13 Pro Max [en_AU, iphone-13-pro-max, dark, home].png"
+magick "iPhone 13 Pro Max [en_AU, iphone-13-pro-max, dark, home] (1).png" -resize 2778x1284\! -alpha off "resized/iPhone 13 Pro Max [en_AU, iphone-13-pro-max, dark, home] (1).png"
+
+magick "Large [en_AU, large, light, home].png" "resized/Large [en_AU, large, light, home].png"
+magick "Large [en_AU, large, light, home] (1).png" "resized/Large [en_AU, large, light, home] (1).png"
+magick "Large [en_AU, large, dark, home].png" "resized/Large [en_AU, large, dark, home].png"
+magick "Large [en_AU, large, dark, home] (1).png" "resized/Large [en_AU, large, dark, home] (1).png"
+
+magick "Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, light, home].png" -resize x3760 "resized/Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, light, home].png"
+magick "Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, light, home] (1).png" -resize 3760x "resized/Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, light, home] (1).png"
+magick "Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, dark, home].png" -resize x3760 "resized/Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, dark, home].png"
+magick "Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, dark, home] (1).png" -resize 3760x "resized/Samsung Galaxy Note 20 Ultra [en_AU, samsung-galaxy-note20-ultra, dark, home] (1).png"
+
+magick "Small [en_AU, small, light, home].png" "resized/Small [en_AU, small, light, home].png"
+magick "Small [en_AU, small, light, home] (1).png" "resized/Small [en_AU, small, light, home] (1).png"
+magick "Small [en_AU, small, dark, home].png" "resized/Small [en_AU, small, dark, home].png"
+magick "Small [en_AU, small, dark, home] (1).png" "resized/Small [en_AU, small, dark, home] (1).png"
+
+magick "Sony Xperia 1 II [en_AU, sony-xperia-1-ii, light, home].png" -resize 1640x3760\! "resized/Sony Xperia 1 II [en_AU, sony-xperia-1-ii, light, home].png"
+magick "Sony Xperia 1 II [en_AU, sony-xperia-1-ii, light, home] (1).png" -resize 3760x1640\! "resized/Sony Xperia 1 II [en_AU, sony-xperia-1-ii, light, home] (1).png"
+magick "Sony Xperia 1 II [en_AU, sony-xperia-1-ii, dark, home].png" -resize 1640x3760\! "resized/Sony Xperia 1 II [en_AU, sony-xperia-1-ii, dark, home].png"
+magick "Sony Xperia 1 II [en_AU, sony-xperia-1-ii, dark, home] (1).png" -resize 3760x1640\! "resized/Sony Xperia 1 II [en_AU, sony-xperia-1-ii, dark, home] (1).png"
