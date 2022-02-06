984d1d0d65224289151cad6eef59485d1817b06c
Brad Rushworth <brushworth@bitbot.com.au>
2022-02-07 00:00:08 +1100

    Fixing broken test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,16 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nuptialflight/main.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
+  PackageInfo.setMockInitialValues(
+      appName: 'appName',
+      packageName: 'packageName',
+      version: 'version',
+      buildNumber: 'buildNumber',
+      buildSignature: 'buildSignature');
+
   testWidgets('Smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyMaterialApp());
@@ -24,5 +32,5 @@ void main() {
     // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
     // expect(find.text('1'), findsOneWidget);
-  });
+  }, timeout: Timeout(Duration(minutes: 2)));
 }
